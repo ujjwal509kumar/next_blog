d93121a2d9c3ad433160e9fa810ee3ad68515290
Ujjwal Kumar <68781093+ujjwal509kumar@users.noreply.github.com>
2024-01-05 18:57:30 +0530

    Update route.js

--- app/api/getblog/route.js
+++ app/api/getblog/route.js
@@ -6,7 +6,7 @@ export async function GET(req) {
         const { searchParams } = new URL(req.url);
         const blogquery = searchParams.get("page");
 
-        const data = await fs.promises.readFile(`app/blogdata/${blogquery}.json`, "utf8");
+        const data = await fs.promises.readFile(`./app/blogdata/${blogquery}.json`, "utf8");
         const parsedData = JSON.parse(data);
         return NextResponse.json(parsedData);
     } catch (err) {
