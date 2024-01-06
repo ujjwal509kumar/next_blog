f73864c589056021b20efe0f6ef181a3b9425da9
Ujjwal Kumar <68781093+ujjwal509kumar@users.noreply.github.com>
2024-01-06 19:44:29 +0530

    Update route.js

--- app/api/getblog/route.js
+++ app/api/getblog/route.js
@@ -1,3 +1,24 @@
+import { NextResponse } from "next/server";
+import * as fs from "fs";
+
+export async function GET(req) {
+    try {
+        const { searchParams } = new URL(req.url);
+        const blogquery = searchParams.get("page");
+
+        const data = await fs.promises.readFile(`./app/blogdata/${blogquery}.json`, "utf8");
+        const parsedData = JSON.parse(data);
+        return NextResponse.json(parsedData);
+    } catch (err) {
+        console.error("Error reading file:", err);
+        return NextResponse.json({ error: "No such blog found" },
+            { status: 500 }
+        );
+    }
+}
+
+
+
 // import { NextResponse } from "next/server";
 // import * as fs from "fs";
 // import { URL } from "url";
@@ -18,21 +39,3 @@
 //         return NextResponse.json({ error: "No such blog found" }, { status: 500 });
 //     }
 // }
-import { NextResponse } from "next/server";
-import * as fs from "fs";
-
-export async function GET(req) {
-    try {
-        const { searchParams } = new URL(req.url);
-        const blogquery = searchParams.get("page");
-
-        const data = await fs.promises.readFile(`./app/blogdata/${blogquery}.json`, "utf8");
-        const parsedData = JSON.parse(data);
-        return NextResponse.json(parsedData);
-    } catch (err) {
-        console.error("Error reading file:", err);
-        return NextResponse.json({ error: "No such blog found" },
-            { status: 500 }
-        );
-    }
-}
