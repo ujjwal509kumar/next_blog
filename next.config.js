ffc4fd115d8ce91e0e0e68d731e39481cb115d99
Ujjwal Kumar <68781093+ujjwal509kumar@users.noreply.github.com>
2024-01-06 19:23:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,7 @@
-// File: next.config.js
-
-// Define rewrites for API routes
-/** @type {import('next').NextConfig} */
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*', // Match any path after '/api/' and capture it as a parameter
-        destination: 'https://next-blog-eight-rust.vercel.app/api/:path*', // Replace with your actual API endpoint
-      },
-    ];
-  },
-};
-
-// Additional Next.js configuration
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true }, // Enable unoptimized image loading
+    images: { unoptimized: true },
 }
 
-// Export the combined configuration
-module.exports = nextConfig;
+module.exports = nextConfig
+
