b837f8829d4562324f1853a290e8646d7ba2c1ff
Ujjwal Kumar <ujjwal509kumar@gmail.com>
2024-04-07 15:23:22 +0530

    Indentation

--- app/api/getblog/route.js
+++ app/api/getblog/route.js
@@ -6,12 +6,12 @@ export async function GET(req) {
         const { searchParams } = new URL(req.url);
         const blogquery = searchParams.get("page");
 
-        const data = await fs.promises.readFile(`app/blogdata/${blogquery}.json`, "utf8");
+        const data = await fs.promises.readFile(`./app/blogdata/${blogquery}.json`, "utf8");
         const parsedData = JSON.parse(data);
         return NextResponse.json(parsedData);
     } catch (err) {
         console.error("Error reading file:", err);
-        return NextResponse.json({ error: "No such blog found" },
+        return NextResponse.json({ error: "No such blog found",err },
             { status: 500 }
         );
     }
